cdb328f0bd9bc3248e2ade6fa16510f17a69abc3
FernandoM52 <fernandomartins.contato@outlook.com>
2023-05-31 19:25:11 -0300

    feat/ trendings-routes - Função para pegar posts por hashtag

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
+import trendingRouter from "./trendings.routes.js";
 
 const router = Router();
 router.use(authRoutes);
+router.use(trendingRouter)
 
 export default router;
