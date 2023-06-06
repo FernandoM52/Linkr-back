36161ae404138d583277ebbbf9980258d70a23dd
lbritors <britoleticiars@gmail.com>
2023-06-06 20:57:18 -0300

    fix: delete post

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { tokenValidate } from "../middlewares/tokenValidate.middleware.js";
-import { newPost, getPost, likePost, getPostById } from "../controllers/post.controllers.js";
+import { newPost, getPost, likePost, getPostById, deletePost } from "../controllers/post.controllers.js";
 
 const postRoutes = Router();
 
@@ -8,5 +8,6 @@ postRoutes.post("/home", tokenValidate, newPost);
 postRoutes.get("/home", getPost);
 postRoutes.get("/posts/:id",getPostById)
 postRoutes.post("/like/:postId", tokenValidate, likePost);
+postRoutes.delete("home:id", tokenValidate, deletePost);
 
 export default postRoutes;
