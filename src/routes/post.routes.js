e89a411220e07d5194681ed033586326ae611d73
Joao Bosco <jboscos.carvalho@gmail.com>
2023-06-06 21:15:04 -0300

    fix: route delete com /

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { tokenValidate } from "../middlewares/tokenValidate.middleware.js";
-import { newPost, getPost, likePost, getPostById, deletePost } from "../controllers/post.controllers.js";
+import {
+  newPost,
+  getPost,
+  likePost,
+  getPostById,
+  deletePost,
+} from "../controllers/post.controllers.js";
 
 const postRoutes = Router();
 
 postRoutes.post("/home", tokenValidate, newPost);
 postRoutes.get("/home", getPost);
-postRoutes.get("/posts/:id",getPostById)
+postRoutes.get("/posts/:id", getPostById);
 postRoutes.post("/like/:postId", tokenValidate, likePost);
-postRoutes.delete("home:id", tokenValidate, deletePost);
+postRoutes.delete("/home/:id", tokenValidate, deletePost);
 
 export default postRoutes;
