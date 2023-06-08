d20d169f7c319195c346b21213c531ae3c10336c
Joao Bosco <jboscos.carvalho@gmail.com>
2023-06-07 23:47:50 -0300

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,12 +2,16 @@ import { db } from "../database/db.connection.js";
 
 export async function searchUser(req, res) {
   const { name } = req.params;
+  const queryName = name.replace(`${name}`, `%${name}%`);
+
   try {
-    await db.query(
-      `SELECT * FROM users WHERE name LIKE $1;
+    const matchUsersList = await db.query(
+      `SELECT users.id, users.name, users.photo  FROM users WHERE upper(name) LIKE upper($1);
     `,
-      [name]
+      [queryName]
     );
+
+    res.send(matchUsersList.rows);
   } catch (err) {
     return res.status(500).send(err.message);
   }
